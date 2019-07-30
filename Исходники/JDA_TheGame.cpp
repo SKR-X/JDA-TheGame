be67fd0aaf78044670a68b30eea780f1ae5eb826
SKR-X <42437146+SKR-X@users.noreply.github.com>
2019-07-30 20:02:30 +0300

    Update JDA_TheGame.cpp

--- Исходники/JDA_TheGame.cpp
+++ Исходники/JDA_TheGame.cpp
@@ -7,40 +7,44 @@
 #include "JDA_obj.h";
 
 
-int main()
+int
+main ()
 {
-	Player Player;
-	string choise = Player.choise;
-	Game Game;
-	Game.intro();
-	Game.start();
-	Player.name = Game.playername;
-	cout << endl;
-	Player.info();
-	cout << endl;
-	Game.select();
-	if (Game.firststage == "Shop") {
-		Game.firststage = "";
-		cout << endl;
-		cout << "  ---TIP TIME!--- " << endl;
-		cout << endl;
-		Sleep(1500);
-		cout << " In this game you need to make choices so choose properly!" << endl;
-		cout << " Good luck!";
-		Sleep(6000);
-		system("cls");
-		cout << endl;
-		cout << " - Hi stranger! Can you give me some money?" << endl;
-		cout << " A - \"Okay ( - 5 from cash  )\" " << endl;
-		cout << " B - *Just go away from him*" << endl;
-		cout << " C - \"Hey, go away\" " << endl;
-		cout << endl;
-		cout << " ";
-		cin >> choise;
-		if (choise=="A") {
-			Player.del_cash(5);
-		}
+  Player Player;
+  string choise = Player.choise;
+  Game Game;
+  Game.intro ();
+  Game.start ();
+  Player.name = Game.playername;
+  cout << endl;
+  Player.info ();
+  cout << endl;
+  Game.select ();
+  if (Game.firststage == "Shop")
+    {
+      Game.firststage = "";
+      cout << endl;
+      cout << "  ---TIP TIME!--- " << endl;
+      cout << endl;
+      Sleep (1500);
+      cout << " In this game you need to make choices so choose properly!" <<
+	endl;
+      cout << " Good luck!";
+      Sleep (6000);
+      system ("cls");
+      cout << endl;
+      cout << " - Hi stranger! Can you give me some money?" << endl;
+      cout << " A - \"Okay ( - 5 from cash  )\" " << endl;
+      cout << " B - *Just go away from him*" << endl;
+      cout << " C - \"Hey, go away\" " << endl;
+      cout << endl;
+      cout << " ";
+      cin >> choise;
+      if (choise == "A")
+	{
+	  Player.del_cash (5);
 	}
-	system("pause");
-	return 0;
+    }
+  system ("pause");
+  return 0;
 }
